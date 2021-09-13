a92d0712ccca477bf466f457418348eb6bdc6ff2
Anoja Kumudunee Somasiri <49120359+kumudunee@users.noreply.github.com>
2021-09-13 09:52:44 +0530

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Lets Code Together'
+          ),
+        ),
+        body: Column(
+          children: [
+            Center(
+
+              child: Container(
+              ),
+
+            ),
+            FlatButton(
+              child: Text("Flat Button",style: TextStyle(fontSize: 20,),),
+              color: Colors.red,
+              textColor: Colors.white,
+              onPressed: (){},
+            ),
+
+           //Raised Button
+            RaisedButton(
+              child: Text("Raised Button",style: TextStyle(fontSize: 20),),
+                color: Colors.blue,
+                splashColor: Colors.green,
+                onPressed: (){}
+            ),
+            //Floating Action button
+            FloatingActionButton(
+              child: Text("Float"),
+                onPressed: (){}
+            ),
+            FloatingActionButton(
+                child: Icon(Icons.print),
+                foregroundColor: Colors.black,
+                backgroundColor: Colors.yellow,
+                onPressed: (){}
+            ),
+            FloatingActionButton.extended(
+                onPressed: () {},
+                label: Text("Print"), icon: Icon(Icons.print),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
